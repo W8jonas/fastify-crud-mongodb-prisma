1a922f807f94fafed5c428b7a87f954c58a757d8
W8jonas <jonashenrique989@gmail.com>
2024-04-05 21:31:15 -0300

    adiciona rotas

--- source/server.ts
+++ source/server.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config'
+
 import fastify from "fastify";
+import {dbConnector} from "./dbConnector";
+import routes from "./routes";
 
 const app = fastify({
   logger: true,
 });
+
+app.register(dbConnector);
+app.register(routes);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("listening on port 3333");
