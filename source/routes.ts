6f6b5ffa52052444c0ccc3c55d63a1ddb9bd21e2
W8jonas <jonashenrique989@gmail.com>
2024-04-06 01:12:31 -0300

    refatora rotas

--- source/routes.ts
+++ source/routes.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from "fastify";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 async function routes(fastify: FastifyInstance) {
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient();
 
   const collection = fastify?.mongo?.db?.collection("Animals");
 
-  if (!collection) return 
+  if (!collection) return;
 
   const animalBodyJsonSchema = {
     type: "object",
@@ -21,25 +21,18 @@ async function routes(fastify: FastifyInstance) {
   };
 
   fastify.post("/animals", { schema }, async (request, reply) => {
-    
-    const {animal} = request.body as any
+    const { animal } = request.body as any;
 
-    try {
-      const result = await prisma.animals.create({
-        data: {
-          animal: animal
-        }
-      })
-      return result;
-    } catch (error) {
-      console.error('error =======>>> ', error)      
-    }
+    const result = await prisma.animals.create({
+      data: {
+        animal: animal,
+      },
+    });
+    return result;
   });
 
-
   fastify.get("/animals", async (request, reply) => {
-
-    const result = await prisma.animals.findMany()
+    const result = await prisma.animals.findMany();
 
     if (result.length === 0) {
       throw new Error("No animals found");
@@ -47,20 +40,18 @@ async function routes(fastify: FastifyInstance) {
     return result;
   });
 
-
   fastify.get("/animals/:animalId", async (request, reply) => {
+    const { animalId } = request.params as any;
 
-    const {animalId}= request.params as any
- 
     if (!animalId) {
       throw new Error("No animalId");
     }
 
     const result = await prisma.animals.findUnique({
       where: {
-        id: animalId
-      }
-    })
+        id: animalId,
+      },
+    });
 
     if (!result) {
       throw new Error("No animal found");
@@ -68,7 +59,6 @@ async function routes(fastify: FastifyInstance) {
 
     return result;
   });
-
 }
 
 export default routes;
