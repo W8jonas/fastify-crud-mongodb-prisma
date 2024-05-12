e23c99d8a06b526d303e6033ded3965737cf106b
W8jonas <jonashenrique989@gmail.com>
2024-05-11 23:58:21 -0300

    Inicio de teste POST /user - deve criar um novo usuário

--- source/index.test.ts
+++ source/index.test.ts
@@ -18,9 +18,26 @@ describe('Testes de integração para as rotas de usuário', () => {
     })
 
     const responseParsed = response.json()
-    console.log('responseParsed', responseParsed)
 
     expect(response.statusCode).toBe(201);
+    expect(responseParsed?.users).toBeInstanceOf(Array);
+  });
+
+  test('POST /user - deve criar um novo usuário', async () => {
+    const response = await fastify.inject({
+      method: 'POST',
+      url: '/user',
+      payload: {
+        name: 'test',
+        email: 'test@test.com',
+        password: 'test',
+      }
+    })
+
+    const responseParsed = response.json()
+    console.log('responseParsed', responseParsed)
+
+    expect(response.statusCode).toBe(200);
     expect(responseParsed?.users).toBeInstanceOf(Array);
   });
 });
